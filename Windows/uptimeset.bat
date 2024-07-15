314ae118d98d0908071cbe0565de042303df2755
meeaehlabs <142396448+meeaehlabs@users.noreply.github.com>
2024-07-15 17:33:13 +0100

    updated to unlock trigger for tasks

--- Windows/uptimeset.bat
+++ Windows/uptimeset.bat
@@ -6,11 +6,10 @@ powershell.exe -Command "Set-ExecutionPolicy Bypass -Scope Process -Force"
 mkdir C:\Uptime
 copy C:\Windows\Temp\uptime.ps1 C:\Uptime\
 
-schtasks /create /tn "RunUptimeScript" /tr "powershell.exe -ExecutionPolicy Bypass -File C:\Uptime\uptime.ps1" /sc daily /st 09:00 /ru System
+powershell.exe -Command "schtasks /create /tn 'RunUptimeScript' /tr 'powershell.exe -ExecutionPolicy Bypass -File C:\Uptime\uptime.ps1' /sc onevent /ec System /mo ' '*[System[Provider[@Name='Microsoft-Windows-Security-Auditing'] and (EventID=4801)]]' /ru System"
 
 echo. > C:\Windows\Temp\uptimelog.txt
 
 echo Batch script executed successfully.
 
 pause
-
